7e800a381f5d311a7c8bb13544295570b9d7b9de
ismail en-niou <132809252+ismail-en-niou@users.noreply.github.com>
2024-03-24 23:24:22 +0000

    test 1

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-
-// Import bcrypt for password hashing
+const { initializeApp } = require("firebase/app");
+const { getDatabase, ref, push } = require("firebase/database");
 const bcrypt = require('bcrypt');
 const express = require("express");
-
 const firebaseConfig = {
   apiKey: "AIzaSyCrJsBOKV-pcUUGL5I5etcxd9WNDMFVTPY",
   authDomain: "my-data-base-54f9a.firebaseapp.com",
@@ -13,40 +12,26 @@ const firebaseConfig = {
   appId: "1:268612316568:web:0f98c5a375710722a1cfed",
   measurementId: "G-VS4P08TY15"
 };
-
-
+const app = initializeApp(firebaseConfig);
+const db = getDatabase();
 const expressApp = express();
 expressApp.use(express.json());
-
-// Create a database array to store users temporarily (replace with Firebase later)
-const users = [];
-
-// Endpoint to handle user registration
 expressApp.post("/register", async (req, res) => {
     try {
-        const { email, password } = req.body;
-        
-        // Check if user already exists
-        const existingUser = users.find((user) => user.email === email);
-        if (existingUser) {
-            return res.status(400).send("User already exists");
-        }
-        
-        // Hash the password
+        const { user, password } = req.body;
+        console.log(req.body)
         const hashedPassword = await bcrypt.hash(password, 10);
-        
-        // Save user data to the database (replace with Firebase)
-        users.push({ email, password: hashedPassword });
-        
-        // Respond with success message
+        const usersRef = ref(db,`/${user}`);
+        await push(usersRef, {
+            user,
+            password: hashedPassword
+        });   
         res.status(201).send("User registered successfully");
     } catch (error) {
-        // Handle errors
         console.error("Error registering user:", error);
         res.status(500).send({ message: "Internal Server Error" });
     }
 });
-
 
 expressApp.listen(3000, () => {
     console.log("Server is running on port 3000");
