e92352258d656754325a4ab9d00ab8bebb80c67d
ismail en-niou <132809252+ismail-en-niou@users.noreply.github.com>
2024-03-25 00:03:37 +0000

    akhirane

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { initializeApp } = require("firebase/app");
-const { getDatabase, ref, push } = require("firebase/database");
+const { getDatabase, ref, get, set } = require("firebase/database");
 const bcrypt = require('bcrypt');
 const express = require("express");
 const firebaseConfig = {
@@ -19,25 +19,46 @@ expressApp.use(express.json());
 expressApp.post("/register", async (req, res) => {
     try {
         const { user, password } = req.body;
-        
-        // Check if user already exists
-        const usersRef = ref(db, "/");
+        const usersRef = ref(db, `/${user}`);
         const snapshot = await get(usersRef);
+        
         if (snapshot.exists()) {
-            const users = snapshot.val();
-            if (users[user]) {
-                return res.status(400).send("User already exists");
-            }
+            res.status(400).send("User already exists");
+        } else {
+            const hashedPassword = await bcrypt.hash(password, 10);
+            await set(ref(db, `/${user}`), {
+                user,
+                password: hashedPassword
+            });   
+            res.status(201).send("User registered successfully");
         }
-
-        const hashedPassword = await bcrypt.hash(password, 10);
-        await set(ref(db, `/${user}`), {
-            user,
-            password: hashedPassword
-        });   
-        res.status(201).send("User registered successfully");
     } catch (error) {
         console.error("Error registering user:", error);
         res.status(500).send({ message: "Internal Server Error" });
     }
+});
+expressApp.post("/login", async (req, res) => {
+    try {
+        const { user, password } = req.body;
+        const userRef = ref(db, `/${user}`);
+        const userSnapshot = await get(userRef);
+        
+        if (!userSnapshot.exists()) {
+            return res.status(400).send("User not found");
+        }
+        const userData = userSnapshot.val();
+        const hashedPassword = userData.password;
+
+        const passwordMatch = await bcrypt.compare(password, hashedPassword);
+        if (!passwordMatch) {
+            return res.status(401).send("Incorrect password");
+        }
+        res.status(200).send("Login successful");
+    } catch (error) {
+        console.error("Error logging in user:", error);
+        res.status(500).send({ message: "Internal Server Error" });
+    }
+});
+expressApp.listen(3000, () => {
+    console.log("Server is running on port 3000");
 });
