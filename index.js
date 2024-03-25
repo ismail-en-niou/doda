2427f0a933945eb120dd3909283e5772b47b2e22
ismail en-niou <132809252+ismail-en-niou@users.noreply.github.com>
2024-03-25 12:22:55 +0000

    akhire mise

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const app = initializeApp(firebaseConfig);
 const db = getDatabase();
 const expressApp = express();
 
-// Use CORS middleware
+
 expressApp.use(cors());
 expressApp.use(express.json());
 
@@ -30,18 +30,18 @@ expressApp.post("/register", async (req, res) => {
         const snapshot = await get(usersRef);
         
         if (snapshot.exists()) {
-            res.status(400).send("User already exists");
+            res.status(400).send({ statut: "1", message: "User already exists" });
         } else {
             const hashedPassword = await bcrypt.hash(password, 10);
             await set(ref(db, `/${user}`), {
                 user,
                 password: hashedPassword
             });   
-            res.status(201).send("User registered successfully");
+            res.status(201).send({ statut: "0", message: "User registered successfully" });
         }
     } catch (error) {
         console.error("Error registering user:", error);
-        res.status(500).send({ message: "Internal Server Error" });
+        res.status(500).send({ statut: "1", message: "Internal Server Error" });
     }
 });
 
@@ -52,22 +52,26 @@ expressApp.post("/login", async (req, res) => {
         const userSnapshot = await get(userRef);
         
         if (!userSnapshot.exists()) {
-            return res.status(400).send("User not found");
+            return res.status(400).send({ statut: "1", message: "User not found" });
         }
         const userData = userSnapshot.val();
         const hashedPassword = userData.password;
 
         const passwordMatch = await bcrypt.compare(password, hashedPassword);
         if (!passwordMatch) {
-            return res.status(401).send("Incorrect password");
+            return res.status(401).send({
+                statut: "1",
+                message: "Incorrect password"
+            });
         }
-        res.status(200).send("Login successful");
+        res.status(200).send({ statut: "0", message: "Login successful" });
     } catch (error) {
         console.error("Error logging in user:", error);
-        res.status(500).send({ message: "Internal Server Error" });
+        res.status(500).send({ statut: "1", message: "Internal Server Error" });
     }
 });
 
 expressApp.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
+
